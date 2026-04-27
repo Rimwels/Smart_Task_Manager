02c65127bf9c3595a71d4ee69c9e4cc32a606a17
Rimwels <0323-3653@lspu.edu.ph>
2026-04-27 11:50:26 +0000

    CORS added

--- app/main.py
+++ app/main.py
@@ -2,11 +2,21 @@ from fastapi import FastAPI
 from app.routes.auth_route import router as auth_router
 from app.routes.task_route import router as task_router
 from app.core.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # tighten later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(task_router)
